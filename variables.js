7f5fc14c8b7477655c8af8a58e29f1d92103d3e0
M Ali R <maliramzan314@gmail.com>
2024-05-16 04:41:19 -0700

    commit

---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345;
+
+let account_Name = "User";
+
+var account_Password = "12345";
+
+account_City = "Lahore";
+
+// Changing values of all variables
+
+// accountId = 67890; // It is not allowed
+
+account_Name = "Admin";
+
+account_Password = "7890";
+
+account_City = "Islamabad";
+
+console.table([accountId, account_Name, account_Password, account_City]);
